d08df1a531952000c32172cf07d5ec62c144b3cc
yeonnnee <61894688+yeonnnee@users.noreply.github.com>
2021-12-19 19:40:07 +0900

    nav 렌더링 조건 수정

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from 'src/service/auth-service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'lemonade';
+  isAuthenticated: boolean = false;
+
+  constructor(private authService:AuthService) {
+  }
+
+  ngOnInit() {
+    this.isAuthenticated = this.authService.userInfo ? true : false;
+  }
 }
