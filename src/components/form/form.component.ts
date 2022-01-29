f2320ce8cedb6614411da1aad466278509559190
yeonnnee <61894688+yeonnnee@users.noreply.github.com>
2022-01-29 21:23:38 +0900

    portInfo 생성

--- src/components/form/form.component.ts
+++ src/components/form/form.component.ts
@@ -1,7 +1,7 @@
 import { moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
 import { Component, OnInit } from '@angular/core';
 import { GridIndex } from '../menu/menu.component';
-import { AddressInfoSection, packingList } from './mockData';
+import { PackingListInfo, packingList } from './mockData';
 
 @Component({
   selector: 'app-form',
@@ -10,18 +10,32 @@ import { AddressInfoSection, packingList } from './mockData';
 })
 export class FormComponent implements OnInit {
 
-  addressInfo: AddressInfoSection[] = [];
-  portInfo: any[] = [];
-  departureInfo: any[] = [];
-  others: any[] = [];
+  addressInfo: PackingListInfo[] = [];
+  portInfo: PackingListInfo[] = [];
+  departureInfo: PackingListInfo[] = [];
+  others: PackingListInfo[] = [];
 
-
+  exceptionList:PackingListInfo[] = [];
   constructor() { }
 
   ngOnInit(): void {
     const data = packingList;
-    this.addressInfo = data.filter(d => d.category === 'address');
-    console.log(this.addressInfo)
+    this.seperateData(data);
   }
 
+  seperateData(data: PackingListInfo[]) {
+    this.addressInfo = data.filter(d => d.category === "address");
+    this.portInfo = data.filter(d => d.category === "portInfo");
+    this.departureInfo = data.filter(d => d.category === "departureInfo");
+    this.others = data.filter(d => d.category !== "departureInfo" && d.category !== "address" && d.category !== 'portInfo');
+  }
+
+  disablePortInfoColumn(item:PackingListInfo) {
+    this.exceptionList.push(item);
+    this.portInfo = this.portInfo.filter(d => d.title !== item.title);
+  }
+  activePortInfoColumn(item: PackingListInfo) {
+    this.exceptionList = this.exceptionList.filter(d => d.title !== item.title);
+    this.portInfo.push(item)
+  }
 }
