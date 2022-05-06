810dc3cd7d6a0d4b3f4d38605f959749453224c0
yeonnnee <61894688+yeonnnee@users.noreply.github.com>
2022-05-06 22:39:41 +0900

    input 선택시 form 값 세팅

--- src/components/form/form.component.ts
+++ src/components/form/form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { AbstractControl, FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormArray, FormArrayName, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
@@ -10,7 +10,7 @@ export class FormComponent {
   statusItems: string[] = ['valid', 'invalid', 'touched', 'dirty', 'pristine', 'pending'];
 
   createFormInfo: FormGroup = new FormGroup({});
-  generatedForms: FormGroup = new FormGroup({});
+  generatedForms: FormGroup = new FormGroup({forms: new FormArray([])});
 
   constructor() {
     this.createFormInfo = new FormGroup({
@@ -20,21 +20,30 @@ export class FormComponent {
         labelName: new FormControl(null),
         placeHolder: new FormControl(null),
         defaultValueTxt: new FormControl(null),
+        valueTxt: new FormControl(null),
         isChecked: new FormControl(false),
         validation: new FormGroup({
           isRequired: new FormControl(false),
-          isEditable: new FormControl(true),
+          isReadOnly: new FormControl(false),
           minLength: new FormControl(null),
           maxLength: new FormControl(null)
         })
       })
     });
-
-    this.generatedForms = new FormGroup({
-      forms: new FormArray([])
-    });
   }
 
+
+  get forms() {
+    return this.generatedForms.get('forms') as FormArray;
+  }
+  get formInfoByType() {
+    const formInfoByType = this.createFormInfo.get('formInfoByType') as FormGroup;
+    return formInfoByType;
+  }
+
+  get validationFormGroup() {
+    return this.formInfoByType.get('validation') as FormGroup;
+  }
 
   selectFormTypeAsInput() {
     this.createFormInfo.get('formType')?.setValue('input');
@@ -48,10 +57,34 @@ export class FormComponent {
     this.createFormInfo.get('formType')?.setValue('radio');
   }
 
+  checkRequired() {
+    const target = document.getElementById('required-input') as HTMLInputElement;
+    console.log(target.checked)
+
+      this.validationFormGroup.get('isRequired')?.setValue(!target.checked);
+
+    console.log(this.validationFormGroup.get('isRequired')?.value);
+  }
+
+  checkReadOnly() {
+    const target = document.getElementById('read-only-input') as HTMLInputElement;
+    console.log(target.checked)
+    if (target.checked) {
+      this.validationFormGroup.get('isReadOnly')?.setValue(true);
+      target.checked = true;
+
+    } else {
+      this.validationFormGroup.get('isReadOnly')?.setValue(false);
+      target.checked = false;
+
+    }
+  }
+
   // TODO: 입력한 양식대로 form 생성
   createForm() {
     const forms = this.generatedForms.get('forms') as FormArray;
     forms.push(this.createFormInfo);
+    console.log(forms.controls[0].get('formTitle'))
   }
 
   // TODO: 생성한 모든 폼 touched 상태로
